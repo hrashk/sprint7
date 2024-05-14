4dc7d803b3fa9366afb29eb4a8b035d19b376ca0
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2024-05-14 19:20:34 +0200

    read config from file

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -3,11 +3,31 @@ package org.example;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import static io.restassured.RestAssured.given;
 
 public class Client {
-    protected static final String BASE_URI = "https://qa-scooter.praktikum-services.ru";
-    private static final String BASE_PATH = "/api/v1";
+    static {
+        loadConfig();
+    }
+
+    protected static final String BASE_URI = System.getProperty("base.uri");
+    private static final String BASE_PATH = System.getProperty("base.path");
+
+    private static void loadConfig() {
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+
+        String fileName = System.getProperty("environment") + ".properties";
+        System.out.println("read config from " + fileName);
+
+        try (InputStream resource = contextClassLoader.getResourceAsStream(fileName)) {
+            System.getProperties().load(resource);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public RequestSpecification spec() {
         return given().log().all()
--- src/main/java/org/example/courier/Courier.java
+++ src/main/java/org/example/courier/Courier.java
@@ -1,50 +1,24 @@
 package org.example.courier;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import org.apache.commons.lang3.RandomStringUtils;
 
+@Data
+@AllArgsConstructor
+@Builder
 public class Courier {
     private String login;
     private String password;
     private String firstName;
-
-    public Courier() {
-    }
-
-    public Courier(String login, String password, String firstName) {
-        this.login = login;
-        this.password = password;
-        this.firstName = firstName;
-    }
+    private String lastName;
 
     public static Courier generic() {
-        return new Courier("Jackey", "P@ssw0rd123", "Sparrow");
+        return new Courier("Jackey", "P@ssw0rd123", "Sparrow", null);
     }
 
     public static Courier random() {
-        return new Courier(RandomStringUtils.randomAlphabetic(5, 15), "P@ssw0rd123", "Sparrow");
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        return new Courier(RandomStringUtils.randomAlphabetic(5, 15), "P@ssw0rd123", "Sparrow", null);
     }
 }
